6721d8306c8c0bcb6a0e2a728e633f231714315e
Abel Cheung <abelcheung@gmail.com>
2024-12-20 04:16:33 +0000

    fix(pyright): Allow diagnostic items containing error to pass schema validation
    So pyright errors are properly reported as TypeCheckerError, not schema validation problem

--- src/pytest_revealtype_injector/adapter/pyright_.py
+++ src/pytest_revealtype_injector/adapter/pyright_.py
@@ -92,6 +92,7 @@ class _PyrightAdapter(TypeCheckerAdapter):
             "start": {"line": int, "character": int},
             "end": {"line": int, "character": int},
         },
+        s.Optional("rule"): str,
     })
 
     @classmethod
